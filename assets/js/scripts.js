6cf28e98a439a0276eb88b78122874fabfa4cbaf
EllenSt264 <stafford.ellen@protonmail.com>
2020-10-18 12:21:22 +0000

    Add emailJS

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -128,3 +128,25 @@ function fetchGitHubInformation(event) {
         }
     )
 }
+
+
+/**
+ * EmailJS
+ */
+
+ function sendMail(contactForm) {
+     emailjs.send("ellenst264", "practice", {
+         /* The keys for this object are equal to the parameter names set in the email template on EmailJS */
+         "from_name": contactForm.name.value,
+         "from_email": contactForm.emailaddress.value,
+         "project_request": contactForm.projectsummary.value
+     })
+     .then (
+         function(response) {
+             console.log("SUCCESS", response);
+         },
+         function(error) {
+             console.log("FAILED", error)
+         }
+     );
+ }
